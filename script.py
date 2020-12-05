b8838b89ae8fcbfb48fcb772585f74352e0634f2
Tomnr100 <62517454+Tomnr100@users.noreply.github.com>
2020-12-05 13:11:11 +0100

    initial commit

---
+++ script.py
@@ -0,0 +1,16 @@
+import math
+import os
+import sys
+import requests
+
+print(sys.version)
+print(sys.executable)
+
+
+def greet(who_to_greet):
+    greeting = 'hello, {}'.format(who_to_greet)
+    return greeting
+
+
+r = requests.get('https://coreyms.com')
+print(r.status_code)
